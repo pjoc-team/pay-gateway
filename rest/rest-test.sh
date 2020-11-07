015e9dda993d6c0df88c9d376467c397e3ad57ab
blademainer <blademainer@gmail.com>
2020-11-08 01:15:40 +0800

    update test

--- rest/rest-test.sh
+++ rest/rest-test.sh
@@ -2,7 +2,7 @@
 export curDir="`dirname $0`"
 #curl -d '{"pay_amount":1, "order_time":"2018-10-28 12:00:00", "app_id":"1", "sign":"3e03ebf6a848b27a75f98f90c0225829", "channel_id":"demo", "sign_type":"MD5"}' "http://127.0.0.1:18080/v1/pay/test"
 
-nowDate="$(date +'%Y-%m-%d %H:%M:%S')"
+nowDate="$(date +'%Y-%m-%dT%H:%M:%S+00:08')"
 now="$(date +'%Y%m%d%H%M%S')"
 orderId="${now}`date +%s%N | awk '{print substr($0,10,9)}'`$RANDOM"
 
@@ -11,6 +11,6 @@ echo "json==$json"
 echo `$curDir/sign -j "$json" -d true` > $curDir/${orderId}.tmp
 sign="`cat $curDir/${orderId}.tmp | awk '{print $NF}'`"
 echo "sign===$sign"
-json='{"pay_amount":1, "out_trade_no": "'${orderId}'", "order_time":"'${nowDate}'", "app_id":"1", "channel_id":"demo", "sign_type":"RSA", "method":"test", "sign":"'$sign'"}'
+json='{"pay_amount":1, "out_trade_no": "'${orderId}'", "order_time":"'${nowDate}'", "app_id":"1", "channel_id":"demo", "sign_type":"RSA", "method":"WEB", "sign":"'$sign'"}'
 echo "final json: $json"
-curl -d "$json" "http://127.0.0.1:18080/v1/pay/test"
+curl -d "$json" "http://127.0.0.1:8080/v1/pay/WEB"
