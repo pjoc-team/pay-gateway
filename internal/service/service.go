2ad2dcd4eeacd8c479071b8d06194e9c85843ed5
blademainer <blademainer@gmail.com>
2020-11-08 16:57:16 +0800

    update service

--- internal/service/service.go
+++ internal/service/service.go
@@ -32,23 +32,29 @@ func (s *services) InitDbClient(ctx context.Context) (pb.PayDatabaseServiceClien
 }
 
 // GetChannelClient get channel client of id
-func (s *services) GetChannelClient(id string) (pb.PayChannelClient, error) {
-	return nil, nil
+func (s *services) GetChannelClient(ctx context.Context, id string) (
+	client pb.PayChannelClient,
+	err error,
+) {
+	_, err = s.initGrpc(
+		ctx, "database-service", func(conn *grpc.ClientConn) interface{} {
+			client = pb.NewPayChannelClient(conn)
+			return client
+		},
+	)
+	return
 }
 
 // GetDatabaseService get channel client of id
 func (s *services) GetDatabaseService(ctx context.Context) (pb.PayDatabaseServiceClient, error) {
-	d, err := grpc.DialContext(
-		ctx, "database-service:8080", grpc.WithChainUnaryInterceptor(
-			tracinggrpc.
-				TracingClientInterceptor(),
-		),
+	var client pb.PayDatabaseServiceClient
+	_, err := s.initGrpc(
+		ctx, "database-service", func(conn *grpc.ClientConn) interface{} {
+			client = pb.NewPayDatabaseServiceClient(conn)
+			return client
+		},
 	)
-	if err != nil {
-		return nil, err
-	}
-	client := pb.NewPayDatabaseServiceClient(d)
-	return client, nil
+	return client, err
 }
 
 func (s *services) initGrpc(
@@ -60,6 +66,14 @@ func (s *services) initGrpc(
 	if err != nil {
 		log.Errorf("failed to get service: %v error: %v", serviceName, err.Error())
 		return nil, err
+	}
+	if svc.Protocol != "" && svc.Protocol != service.GRPC {
+		log.Errorf(
+			"service: %v's protocol is not grpc, "+
+				"actual is: %v and continue try to connect by grpc protocol",
+			serviceName,
+			svc.Protocol,
+		)
 	}
 
 	target, err := svc.BuildTarget(ctx)
