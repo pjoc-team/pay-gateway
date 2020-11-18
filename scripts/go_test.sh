fa27a17691478f6fd51d24345f3df13d547e8b4c
blademainer <blademainer@gmail.com>
2020-11-18 18:17:10 +0800

    add profile test

--- scripts/go_test.sh
+++ scripts/go_test.sh
@@ -3,5 +3,7 @@
 find . -name "*.go" | while read f; do
     echo "${f%/*}"
 done | sort | uniq | while read f; do
-    go test "$f"
+    go test -race $f
+    go test -v -cpuprofile="$f.cpu.prof" -memprofile "$f.mem.prof" -bench -x "$f"
+    go tool pprof -svg "$f.cpu.prof" > $f.prof.svg
 done
