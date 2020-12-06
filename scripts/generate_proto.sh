2de4459ce3b51d7a7144d8dc0724bf1fc0794aac
blademainer <blademainer@gmail.com>
2020-12-06 15:59:22 +0800

    fix stream demo

--- scripts/generate_proto.sh
+++ scripts/generate_proto.sh
@@ -40,7 +40,8 @@ find $PROTO_HOME -name "*.proto" | while read proto; do
   echo "proto file: $proto dir: ${dir} file_name: $file_name proto_name: $proto_name out_dir: ${out_dir}"
   echo "generating proto..."
   [[ "$language" == "go" ]] && addition=" --with-gateway --validate-out lang=go:/out "
-  docker run --rm -v "$dir":/defs -v "${dir}":/out blademainer/protoc-all:latest -i /defs -i /go/src -d /defs/ -l $language -o /out --lint $addition
+#  docker run --rm -v "$dir":/defs -v "${dir}":/out blademainer/protoc-all:latest -i /defs -i /go/src -d /defs/ -l $language -o /out --lint $addition
+  docker run --rm -v "$dir":/defs -v "${dir}":/out namely/protoc-all:latest -f $file_name -l $language -o /out --lint --with-validator $addition
 #  docker run --rm -v $dir:/defs -v ${IMPORT_HOME}:/input blademainer/protoc-all:latest -i /defs -i /input -i /go/src/ -d /defs/ -l go -o /defs --validate-out "lang=go:/defs" --with-gateway --lint $addition
 #  docker run --rm -v "$dir":/defs -v "${out_dir}":/out namely/protoc-all:latest -f ${file_name} -i ${dir} -l $language -o /out --lint --with-validator --validate-out --with-gateway
   addition=""
